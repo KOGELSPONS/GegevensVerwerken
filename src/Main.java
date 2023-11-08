2eb3f76f5cdcfb302b38dabfd3aa3a206ab3e38b
Merijn <merijnvannorden@gmail.com>
2023-11-08 14:20:34 +0100

    Sheesh een enter minder

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 import javax.swing.*;
 class gui{
-    public static void main(String args[]){
+    public static void main(String[] args){
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
         JButton button1 = new JButton("Hoi");
         frame.getContentPane().add(button1);
         frame.setVisible(true);
-
     }
 }
